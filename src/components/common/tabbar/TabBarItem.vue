bf566bbacf435e89ddbf83b790d3a303b9df7b9e
skybluezlm <2213253825@qq.com>
2020-12-12 15:50:40 +0800

    '0-1'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot  name="item-text"></slot></div>
+<!--    <img src="../../assets/img/tabbar/home.svg" alt="">-->
+<!--    <div>首页</div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TarBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive: true,
+    }
+  },
+  computed: {
+    isActive() {
+      //判断当前点击的是否为当前活跃的
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      console.log(this.path)
+
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 4px;
+    vertical-align: middle;
+  }
+
+</style>
